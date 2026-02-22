3d90faf81cf4465deaee7cbb3da1a19672864c2a
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2026-02-22 15:37:28 +0000

    [MetaMask] - Prevent 3rd party popups when connecting wallet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import type { SupportedWalletsMap } from "@/types";
 export const CACHE_DIR_NAME = ".wallet-cache";
 export const WALLET_SETUP_DIR_NAME = "wallet-setup";
 
-const METAMASK_VERSION = "13.15.0";
+const METAMASK_VERSION = "13.19.0";
 const ARCHIVED_WALLET_BASE_URL = `https://github.com/amaify/playwright-kit-web3/releases/download/v0.1.0/`;
 
 export const METAMASK_DOWNLOAD_URL = `https://github.com/MetaMask/metamask-extension/releases/download/v${METAMASK_VERSION}/metamask-chrome-${METAMASK_VERSION}.zip`;
--- src/wallets/metamask/actions/connect-to-app.metamask.ts
+++ src/wallets/metamask/actions/connect-to-app.metamask.ts
@@ -1,5 +1,6 @@
 import { expect, type Page } from "@playwright/test";
-import { sleep } from "@/utils/sleep";
+// import { sleep } from "@/utils/sleep";
+import { MetamaskProfile } from "../metamask-profile";
 import { accountSelectors } from "../selectors/homepage-selectors.metamask";
 import { switchAccount } from "./switch-account.metamask";
 
@@ -36,47 +37,82 @@ export async function connectToApp(page: Page, account?: string) {
     await expect(connectMoreAccountsButton).toBeVisible();
     await connectMoreAccountsButton.click();
 
+    // Now Allow only Ethereum and Sepolia chains
+    const permissionsTab = page.getByRole("tab", { name: "Permissions", exact: true });
+    await permissionsTab.waitFor({ state: "visible", timeout: 30_000 });
+    await permissionsTab.click();
+
+    const updateEnabledNetwork = page.getByTestId("site-cell-connection-list-item").last();
+    const editButton = updateEnabledNetwork.getByRole("button", { name: "Edit", exact: true });
+    await editButton.click();
+
+    const editNetworkDialog = page.locator("section[role='dialog']:has-text('Edit networks')");
+    const selectAllLabel = editNetworkDialog.locator("label:has-text('Select all')");
+    await selectAllLabel.scrollIntoViewIfNeeded();
+    await selectAllLabel.click();
+
+    const ethereumCheckbox = editNetworkDialog.getByTestId("Ethereum");
+    await ethereumCheckbox.scrollIntoViewIfNeeded();
+    await ethereumCheckbox.click();
+
+    const sepoliaCheckbox = editNetworkDialog.getByTestId("Sepolia");
+    await sepoliaCheckbox.scrollIntoViewIfNeeded();
+    await sepoliaCheckbox.click();
+
+    const updateButton = editNetworkDialog.getByRole("button", { name: "Update", exact: true });
+    await updateButton.click();
+
+    // await sleep(400_000);
+
     if (account) {
         await switchAccount({ page, accountName: account });
     }
 
     const connectButton = page.getByRole("button", { name: "Connect", exact: true });
     await connectButton.waitFor({ state: "visible", timeout: 25_000 });
-    await connectButton.click();
 
-    const connectingText = page.getByRole("heading", { name: "Connecting", exact: true });
-    await connectingText.waitFor({ state: "detached", timeout: 25_000 });
+    const metamaskProfile = new MetamaskProfile();
 
-    let isNoticeDialogVisible: boolean | undefined;
+    const promptUrl = await metamaskProfile.promptUrl();
+    const ev = page.waitForEvent("close", { predicate: (page) => !!page.url().match(promptUrl), timeout: 30_000 });
 
-    await expect
-        .poll(
-            async () => {
-                const noticeDialog = page.getByRole("dialog");
-                isNoticeDialogVisible = await noticeDialog.isVisible().catch(() => false);
-                return isNoticeDialogVisible;
-            },
-            {
-                timeout: 25_000,
-            },
-        )
-        .toBe(true)
-        .catch(() => console.error("Notice dialog did not appear within the timeout period."));
+    await Promise.all([connectButton.click(), ev]);
 
-    if (isNoticeDialogVisible) {
-        const snapPrivacyScrollButton = page.getByTestId("snap-privacy-warning-scroll");
-        const acceptButton = page.getByRole("button", { name: "Accept", exact: true });
+    // await connectButton.click();
 
-        await snapPrivacyScrollButton.click();
-        await snapPrivacyScrollButton.waitFor({ state: "detached", timeout: 30_000 });
+    // const connectingText = page.getByRole("heading", { name: "Connecting", exact: true });
+    // await connectingText.waitFor({ state: "detached", timeout: 25_000 });
 
-        await expect(acceptButton).toBeEnabled({ timeout: 30_000 });
-        await acceptButton.click();
-    }
+    // let isNoticeDialogVisible: boolean | undefined;
 
-    const confirmButton = page.getByTestId("page-container-footer-next");
-    await confirmButton.waitFor({ state: "visible", timeout: 25_000 });
-    await confirmButton.click();
+    // await expect
+    //     .poll(
+    //         async () => {
+    //             const noticeDialog = page.getByRole("dialog");
+    //             isNoticeDialogVisible = await noticeDialog.isVisible().catch(() => false);
+    //             return isNoticeDialogVisible;
+    //         },
+    //         {
+    //             timeout: 25_000,
+    //         },
+    //     )
+    //     .toBe(true)
+    //     .catch(() => console.error("Notice dialog did not appear within the timeout period."));
 
-    await sleep(2_000);
+    // if (isNoticeDialogVisible) {
+    //     const snapPrivacyScrollButton = page.getByTestId("snap-privacy-warning-scroll");
+    //     const acceptButton = page.getByRole("button", { name: "Accept", exact: true });
+
+    //     await snapPrivacyScrollButton.click();
+    //     await snapPrivacyScrollButton.waitFor({ state: "detached", timeout: 30_000 });
+
+    //     await expect(acceptButton).toBeEnabled({ timeout: 30_000 });
+    //     await acceptButton.click();
+    // }
+
+    // const confirmButton = page.getByTestId("page-container-footer-next");
+    // await confirmButton.waitFor({ state: "visible", timeout: 25_000 });
+    // await confirmButton.click();
+
+    // await sleep(2_000);
 }
