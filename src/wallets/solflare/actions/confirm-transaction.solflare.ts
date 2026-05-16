ce9a3c9b3b8539dcb59bf090357c211e14d45b59
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2026-05-16 18:18:50 +0100

    [CI] - Fix flakiness in Solflare "confirm-transaction" action

--- src/wallets/solflare/actions/confirm-transaction.solflare.ts
+++ src/wallets/solflare/actions/confirm-transaction.solflare.ts
@@ -4,7 +4,21 @@ import { popupPageSelectors } from "../selectors/popup-page-selectors.solflare";
 export async function confirmTransaction(page: Page) {
     const approveButton = page.getByTestId(popupPageSelectors.approveButton);
     const networkFeeSection = page.getByTestId("section-network-fee");
-    await networkFeeSection.waitFor({ state: "attached", timeout: 40_000 });
+    await networkFeeSection.waitFor({ state: "attached", timeout: 30_000 }).catch(async () => {
+        console.error(
+            "Network fee section did not appear within the timeout period, which may indicate that the transaction confirmation popup did not load correctly.",
+        );
+        const errorTitle = page.getByText("Network mismatch", { exact: true });
+        const isErrorTitleVisible = await errorTitle.isVisible().catch(() => false);
+        if (isErrorTitleVisible) {
+            const closeButton = page.getByRole("button", { name: "Close", exact: true });
+            await closeButton.click();
+            console.error(
+                "A 'Network mismatch' error was detected in the popup. Closing the popup and aborting the transaction confirmation process.",
+            );
+        }
+        return;
+    });
 
     const controlLabelText = page.locator("div[data-id='control-label']");
     const isControlLabelTextVisible = await controlLabelText.isVisible().catch(() => false);
