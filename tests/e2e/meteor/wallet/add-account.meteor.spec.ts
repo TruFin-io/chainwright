682433265825379071c730b5c4e04961dda74246
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2026-06-03 15:16:06 +0100

    [chore] - fix failing CI

--- tests/e2e/meteor/wallet/add-account.meteor.spec.ts
+++ tests/e2e/meteor/wallet/add-account.meteor.spec.ts
@@ -6,6 +6,7 @@ test("should add an account successfully", async ({ meteor }) => {
     await meteor.addAccount({
         accountName: "Alpha",
         network: "Mainnet",
+        mode: "privateKey",
         privateKey: "ed25519:mGMo7jVmHRUtoy88eKHatSQ2Em9pW75bPthTikyc9R5rLUGi7bEdxPwTTNdX5f83PPy3xc5cjbMpZHmnm62fK4J",
     });
 });
