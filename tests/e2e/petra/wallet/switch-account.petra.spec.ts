557c288d8aa805028a1173cb10d30a773ef53944
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2026-06-05 11:03:57 +0100

    Fix failing CI

--- tests/e2e/petra/wallet/switch-account.petra.spec.ts
+++ tests/e2e/petra/wallet/switch-account.petra.spec.ts
@@ -5,19 +5,12 @@ import { accountSelectors } from "@/wallets/petra/selectors/homepage-selectors.p
 const test = testWithPetraFixture;
 
 test("Should switch account successfully", async ({ petra, petraPage }) => {
-    const ACCOUNT_NAME = "Non whitelisted account";
+    const ACCOUNT_NAME = "Echo";
 
     await petra.switchAccount(ACCOUNT_NAME);
 
     const accountMenuButton = petraPage.locator(accountSelectors.accountOptionsMenuButton).first();
-    const accountMenutButtonText = (await accountMenuButton.textContent())
-        ?.split("Switch wallet")[1]
-        ?.split("0x")[0]
-        ?.split("...")[0]
-        ?.trim()
-        .toLowerCase();
+    const accountMenutButtonText = (await accountMenuButton.textContent())?.split("Switch wallet")[1]?.split("0x")[0];
 
-    expect(accountMenutButtonText).not.toBe("");
-    // biome-ignore lint/style/noNonNullAssertion: it shouldn't be undefined
-    expect(ACCOUNT_NAME.toLowerCase().trim().startsWith(accountMenutButtonText!)).toBe(true);
+    expect(accountMenutButtonText?.toLowerCase().trim()).toContain(ACCOUNT_NAME.toLowerCase().trim());
 });
