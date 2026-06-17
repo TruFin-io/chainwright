5ecd9121fae8bc690d9e88b408b8e6b64b923641
Ugwuanyi TobeChukwu <39131739+amaify@users.noreply.github.com>
2026-06-17 12:16:46 +0100

    [Core] - Add support for custom download URL and local extensions

--- src/utils/prepare-wallet-extension.ts
+++ src/utils/prepare-wallet-extension.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { styleText } from "node:util";
 import AdmZip from "adm-zip";
-import type { CLIOptions } from "@/types";
+import type { CLIOptions, ExtensionSource } from "@/types";
 import { SUPPORTED_WALLETS } from "./constants";
 import { downloadFile } from "./download-file";
 import getCacheDirectory from "./get-cache-directory";
@@ -11,17 +11,21 @@ type Args = {
     force: boolean;
     name: CLIOptions;
     downloadUrl: string;
+    extensionSource?: ExtensionSource;
 };
 
 let IS_EXECUTED = false;
 let PREVIOUS_WALLET_NAME = "";
 
-export async function prepareWalletExtension({ downloadUrl, name, force }: Args) {
+export async function prepareWalletExtension({ downloadUrl, name, force, extensionSource }: Args) {
     const CACHE_DIR_NAME = getCacheDirectory(name);
     const supportedWallet = SUPPORTED_WALLETS[name as Exclude<CLIOptions, "all">];
     const walletName = supportedWallet.extensionName;
     const zipFilePath = path.join(CACHE_DIR_NAME, `${name}-extension.zip`);
     const outputPath = path.join(CACHE_DIR_NAME, `${name}-extension`);
+
+    const urlSource = extensionSource && "downloadUrl" in extensionSource ? extensionSource.downloadUrl : downloadUrl;
+    const localPath = extensionSource && "localPath" in extensionSource ? extensionSource.localPath : undefined;
 
     if (!IS_EXECUTED) {
         PREVIOUS_WALLET_NAME = walletName;
@@ -51,19 +55,29 @@ export async function prepareWalletExtension({ downloadUrl, name, force }: Args)
     if (fs.existsSync(outputPath)) {
         console.info(`✅ ${walletName} Version is downloaded already.`);
     } else {
-        console.info(styleText("cyanBright", `📥 Downloading ${walletName} extension...`, { validateStream: false }));
-        await downloadFile({ url: downloadUrl, destination: zipFilePath });
-        console.info(
-            styleText("green", `✅ ${name.toUpperCase()} Extension downloaded successfully.`, {
-                validateStream: false,
-            }),
-        );
+        if (!localPath) {
+            console.info(
+                styleText("cyanBright", `📥 Downloading ${walletName} extension...`, { validateStream: false }),
+            );
+            await downloadFile({ url: urlSource, destination: zipFilePath });
+            console.info(
+                styleText("green", `✅ ${name.toUpperCase()} Extension downloaded successfully.`, {
+                    validateStream: false,
+                }),
+            );
+        } else {
+            console.info(
+                styleText("cyanBright", `Using the local ${name} extension zip file`, {
+                    validateStream: false,
+                }),
+            );
+        }
     }
 
     // Unzip the archive if not already extracted
     if (!fs.existsSync(outputPath)) {
         console.info(`📦 Extracting extension...`);
-        const zip = new AdmZip(zipFilePath);
+        const zip = new AdmZip(!localPath ? zipFilePath : localPath);
         zip.extractAllTo(outputPath, true);
         console.info(`✅ ${walletName} Extension extracted successfully.`);
     } else {
